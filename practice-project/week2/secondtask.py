59492cc97abb9dda79432df4b7f5b7017128dfb2
avashraj <avashraj328@gmail.com>
2024-03-01 11:07:41 -0800

    added more tests but having trouble with testing for typeerror

--- practice-project/week2/secondtask.py
+++ practice-project/week2/secondtask.py
@@ -1,5 +1,7 @@
 
 def isPrime(n):
+    if not isinstance(n, int):
+        raise TypeError
     if n > 1:
         for i in range(2,int(n/2) + 1):
             if (n % i) == 0:
@@ -7,3 +9,5 @@ def isPrime(n):
         return True
     else:
         return False
+
+print(isPrime(1111))
--- practice-project/week2/test_prime.py
+++ practice-project/week2/test_prime.py
@@ -1,16 +1,40 @@
+import pytest
 from secondtask import isPrime
 
-def assertPrime(n):
-    assert isPrime(n) == True
+def assertPrime(n: int, isValidPrime: bool):
+    assert isPrime(n) == isValidPrime
 
-def assertNotPrime(n):
-    assert isPrime(n) == False
-
+#checking prime
 def testPrimeNums():
-    assertPrime(5)
-    assertPrime(3)
-    assertPrime(2)
+    assertPrime(5, True)
+    assertPrime(3, True)
 
+#checking non prime
 def testNonPrime():
-    assertNotPrime(4)
-    assertNotPrime(1)
+    assertPrime(4, False)
+    assertPrime(10, False)
+
+#checking edge cases
+def testEdge():
+    assertPrime(1, False)
+    assertPrime(2, True)
+
+#checking negative
+def testNegative():
+    assertPrime(-1, False)
+    assertPrime(-50, False)
+
+def testLarge():
+    assertPrime(512343, False)
+    assertPrime(1111, False)
+
+#checking types
+    
+''' 
+def testDiffTypes():
+    with pytest.raises(TypeError):
+        isPrime("Hello")
+    with pytest.raises(TypeError):
+        isPrime(True)
+'''
+
