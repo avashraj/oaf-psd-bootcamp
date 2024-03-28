7fa8c53c1dd1b24e7a69278d82830473901f0e07
avashraj <avashraj328@gmail.com>
2024-03-28 11:21:11 -0700

    project layout

---
+++ main-project/prophelper.py
@@ -0,0 +1,20 @@
+'''
+Avash Adhikari
+nba prop helper application (no cool name yet)
+
+started: 3/27/28
+finished:
+
+app needs to:
+take in nba prop: jokic over 10 assists
+respond with:  good pick / bad pick
+
+'''
+
+
+
+
+
+
+
+
